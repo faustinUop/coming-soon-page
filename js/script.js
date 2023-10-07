783c63f09edb5eceede87e2965293b6f8706dada
faustinUop <144625313+faustinUop@users.noreply.github.com>
2023-10-07 15:37:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 
+// JavaScript for email validation and form submission
+document.getElementById('subscribeForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const errorMessage = document.getElementById('error-message');
+
+    if(!email) {
+        errorMessage.textContent = 'Email is required!';
+        return;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if(!emailRegex.test(email)) {
+        errorMessage.textContent = 'Please enter a valid email!';
+        return;
+    }
+
+    errorMessage.textContent = '';
+    alert('Thank you for subscribing!');
+});
